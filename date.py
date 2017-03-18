c8ffcd9d05a3124b3de5dec17c4dc0797e720d84
Arindam Das Modak <arindam.modak03@gmail.com>
2017-03-19 00:45:20 +0530

    Add files via upload

---
+++ date.py
@@ -0,0 +1,24 @@
+import time as t
+from os import path
+
+def createFile(dest):
+    '''
+    the script creates a text fileat the passed in location
+    names file based on date
+    '''
+    
+    date=t.localtime(t.time())
+
+    ##FileName = Month_Day_Year
+    name='%d_%d_%d.txt'%(date[1],date[2],(date[0]%100))
+
+    if not (path.isfile(dest + name)):
+        f=open(dest + name,'w')
+        f.write('\n'*30)
+        f.close()
+
+if __name__=='__main__':
+    destination='C:\\Users\
+\\arindam das modak\\Desktop\\python scripting\\'
+    createFile(destination)
+    raw_input("Done")
